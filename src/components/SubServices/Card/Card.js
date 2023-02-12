81150f132eb8faaf13f5685d76c38a60a57baeb1
hussein <asd>
2023-02-12 17:25:21 +0200

    new commit

--- src/components/SubServices/Card/Card.js
+++ src/components/SubServices/Card/Card.js
@@ -8,23 +8,27 @@ import spiral from "../../../assets/images/spiral1.png";
 
 import Button from '../../Button/Button';
 
-const Card = ({ image, title , text}) => {
+const Card = ({ image, title , text , height}) => {
   const { t } = useTranslation();
 
+  const styles = {
+    height:`${height}`
+  }
+
   return (
-    <div className="card_sub_service">
+    <div className="card_sub_service col-lg-3 mx-3 my-3" style={styles}>
       <div className="head">
         <img src={image} alt="" />
       </div>
       <div className="body">
-        <div>
+        <div className="title">
           <h3>{t(`${title}`)}</h3>
           <img src={spiral} alt="" />
         </div>
         <p>{t(`${text}`)}</p>
       </div>
-      <div>
-        <Button name={"know_more"}/>
+      <div className="footer_card">
+        <Button name={"know_more"} color1={"#39858E"} color2={"#FDFDFE"}/>
       </div>
     </div>
   );
--- src/components/SubServices/SubServices.js
+++ src/components/SubServices/SubServices.js
@@ -1,18 +1,23 @@
 import React, { useContext } from "react";
 
-import './SubServices.scss';
+import "./SubServices.scss";
 
 //** state management */
 import { StateContext } from "../StateProvider";
 
 import { useTranslation } from "react-i18next";
 
-
 //**import components */
-import Card from './Card/Card';
+import Card from "./Card/Card";
 
 //**import images */
-import image1 from '../../assets/images/email_marketing.png';
+import image1 from "../../assets/images/email_marketing.png";
+import image2 from "../../assets/images/google_ads.png";
+import image3 from "../../assets/images/marketing_strategy.png";
+import image4 from "../../assets/images/content_marketing.png";
+import image5 from "../../assets/images/search_engine.png";
+import image6 from "../../assets/images/sms_marketing.png";
+import image7 from "../../assets/images/content_marketing.png";
 
 const SubServices = () => {
   //** this is state to change side rtl and ltr */
@@ -21,11 +26,55 @@ const SubServices = () => {
   const { t } = useTranslation();
 
   return (
-    <div className="sub_services"  dir={`${changeSide === "ar" ? "rtl" : "ltr"}`}>
-        <h2>{t("sub_services")}</h2>
-        <div className="cards">
-            <Card image={image1}/>
-        </div>
+    <div
+      className="sub_services"
+      dir={`${changeSide === "ar" ? "rtl" : "ltr"}`}
+    >
+      <h2>{t("sub_services")}</h2>
+      <div className="cards row">
+        <Card
+          image={image1}
+          title={"email_marketing"}
+          text={"through_email"}
+          height={`${changeSide === "ar" ? "461px" : "525px"}`}
+        />
+        <Card
+          image={image2}
+          title={"google_ads"}
+          text={"through_google"}
+          height={`${changeSide === "ar" ? "461px" : "525px"}`}
+        />
+        <Card
+          image={image3}
+          title={"marketing_strategy"}
+          text={"while_building"}
+          height={`${changeSide === "ar" ? "461px" : "525px"}`}
+        />
+        <Card
+          image={image4}
+          title={"content_marketing"}
+          text={"the_importance"}
+          height={`${changeSide === "ar" ? "461px" : "525px"}`}
+        />
+        <Card
+          image={image5}
+          title={"search_engine"}
+          text={"through_this_service"}
+          height={`${changeSide === "ar" ? "461px" : "525px"}`}
+        />
+        <Card
+          image={image6}
+          title={"sms_marketing"}
+          text={"sms_marketing_"}
+          height={`${changeSide === "ar" ? "461px" : "525px"}`}
+        />
+        <Card
+          image={image7}
+          title={"marketing_consulting"}
+          text={"marketing_consulting_"}
+          height={`${changeSide === "ar" ? "461px" : "525px"}`}
+        />
+      </div>
     </div>
   );
 };
